e896538991111d0317fb0d86877e135f2c4a6526
kent riggs <k2r3@proton.me>
2024-03-16 18:11:30 -0700

    added code to satisfy tests

--- server/app.py
+++ server/app.py
@@ -16,7 +16,6 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
 
     def get(self):
@@ -37,16 +36,29 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
-
 
 class PlantByID(Resource):
 
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
-
+    
+    def patch(self, id):
+        plant = db.session.get(Plant, id)
+        if plant:
+            data = request.get_json()
+            if 'is_in_stock' in data:
+                plant.is_in_stock = data['is_in_stock']
+                db.session.commit()
+                return make_response(jsonify(plant.to_dict()), 200)
+            
+    def delete(self, id):
+        plant = db.session.get(Plant, id)
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response(jsonify({}), 204)        
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
